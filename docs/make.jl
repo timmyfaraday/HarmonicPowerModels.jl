47ab2bbdabe085ee214c6f7e21bbf7c6a767e951
Frederik <frederik.geth@gmail.com>
2021-10-21 15:41:44 +1100

    add pdf compilation

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,32 @@ push!(LOAD_PATH,"../src/")
 using Documenter
 
 makedocs(sitename="HarmonicPowerModels Documentation")
+
+# A flag to check if we are running in a GitHub action.
+const _IS_GITHUB_ACTIONS = get(ENV, "GITHUB_ACTIONS", "false") == "true"
+
+# Pass --pdf to build the PDF. On GitHub actions, we always build the PDF.
+# const _PDF = findfirst(isequal("--pdf"), ARGS) !== nothing || _IS_GITHUB_ACTIONS
+const _PDF = true
+
+if _PDF
+    latex_platform = _IS_GITHUB_ACTIONS ? "docker" : "native"
+    @time Documenter.makedocs(
+        sitename = "HarmonicPowerModels",
+        authors = "The HarmonicPowerModels core developers and contributors",
+        format = Documenter.LaTeX(platform = latex_platform),
+        build = "latex_build",
+    )
+    # Hack for deploying: copy the pdf (and only the PDF) into the HTML build
+    # directory! We don't want to copy everything in `latex_build` because it
+    # includes lots of extraneous LaTeX files.
+    cp(
+        joinpath(@__DIR__, "latex_build", "HarmonicPowerModels.pdf"),
+        joinpath(@__DIR__, "build", "HarmonicPowerModels.pdf"),
+    )
+end
+
+Documenter.deploydocs(
+    repo = "github.com/timmyfaraday/HarmonicPowerModels.jl.git",
+    push_preview = true,
+)
