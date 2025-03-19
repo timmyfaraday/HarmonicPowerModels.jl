bcb18ed7f9531a17b3541bfbf71c6025df12753c
u0099904 <van_acker_tom@me.com>
2025-03-19 09:04:34 +0100

    add. test for xfmr

---
+++ test/data/matpower/xfmr/two_bus_xfmr_YNyn3_hpf.m
@@ -0,0 +1,36 @@
+% two bus network with single branch w/o shunt 
+
+function mpc = two_bus_example
+mpc.version = '2';
+mpc.baseMVA =  1.00;
+
+%% bus data
+%	bus_id	type    Pd      Qd	    Gs	    Bs	    area	Vm	    Va	    baseKV  zone    Vmax	Vmin
+mpc.bus = [
+    1       4       0.000   0.000   0.00    0.00    1       1.00    0.00    10      1       1.10    0.90;
+    2       1       1.000   0.500   0.00    0.00    1       1.00	0.00    10      1       1.10    0.90;
+];
+
+%% bus harmonic data 
+%column_names%  nh_1    nh_2    nh_3    std
+mpc.bus_harmonics = [
+                1.000   0.000   0.000   'IEC61000-2-4:2002, Cl. 2'
+                1.000   0.050   0.050   'IEC61000-2-4:2002, Cl. 2'
+]
+
+%% generator data
+%   bus     Pg      Qg      Qmax    Qmin    Vg      mBase   status  Pmax    Pmin
+mpc.gen = [
+    1       0.0     0.0     9.00    -9.00   1.05    100.0   1       10.00   -10.0;
+];
+
+%% branch data
+%   f_bus	t_bus	r	    x	    b	    rateA	rateB	rateC	ratio	angle	status	angmin	angmax
+mpc.branch = [ 
+];
+
+%% transformer data
+%column_names%  f_bus   t_bus   xsc         gsh         r1          r2          vg      gnd1    gnd2    re1     xe1     re2         xe2         rate_a
+mpc.xfmr = [
+                1       2       0.01        0.0         0.0025      0.0025      'Yy3'   1       1       0.0     0.0     0.0         0.0         10;  
+];
--- test/xfmr.jl
+++ test/xfmr.jl
@@ -41,12 +41,12 @@
         @test sol_pf["bus"]["2"]["vr"] ≈ sol_hpf["bus"]["2"]["vbr"]
         @test sol_pf["bus"]["2"]["vi"] ≈ sol_hpf["bus"]["2"]["vbi"]
      
-        @test sol_pf["branch"]["1"]["cr_fr"]    ≈ sol_hpf["branch"]["1"]["cbr_fr"]
-        @test sol_pf["branch"]["1"]["ci_fr"]    ≈ sol_hpf["branch"]["1"]["cbi_fr"]
-        @test sol_pf["branch"]["1"]["csr_fr"]   ≈ sol_hpf["branch"]["1"]["cbsr_fr"]
-        @test sol_pf["branch"]["1"]["csi_fr"]   ≈ sol_hpf["branch"]["1"]["cbsi_fr"]
-        @test sol_pf["branch"]["1"]["cr_to"]    ≈ sol_hpf["branch"]["1"]["cbr_to"]
-        @test sol_pf["branch"]["1"]["ci_to"]    ≈ sol_hpf["branch"]["1"]["cbi_to"]
+        @test sol_pf["branch"]["1"]["cr_fr"]  ≈ sol_hpf["branch"]["1"]["cbr_fr"]
+        @test sol_pf["branch"]["1"]["ci_fr"]  ≈ sol_hpf["branch"]["1"]["cbi_fr"]
+        @test sol_pf["branch"]["1"]["csr_fr"] ≈ sol_hpf["branch"]["1"]["cbsr_fr"]
+        @test sol_pf["branch"]["1"]["csi_fr"] ≈ sol_hpf["branch"]["1"]["cbsi_fr"]
+        @test sol_pf["branch"]["1"]["cr_to"]  ≈ sol_hpf["branch"]["1"]["cbr_to"]
+        @test sol_pf["branch"]["1"]["ci_to"]  ≈ sol_hpf["branch"]["1"]["cbi_to"]
 
         @test sol_pf["gen"]["1"]["crg"] ≈ sol_hpf["gen"]["1"]["cgr"]
         @test sol_pf["gen"]["1"]["cig"] ≈ sol_hpf["gen"]["1"]["cgi"]
@@ -91,6 +91,109 @@
         
         @test sol_hpf_b["gen"]["1"]["cgr"] ≈ sol_hpf_x["gen"]["1"]["cgr"]
         @test sol_hpf_b["gen"]["1"]["cgi"] ≈ sol_hpf_x["gen"]["1"]["cgi"]
+    end
+
+    @testset "Two bus system, single xfmr (YNyn0), w/wo no load active losses" begin
+        # Example comparing a two bus system with a single xfmr (YNyn0) with and
+        # without no load losses, for the fundamental harmonic
+        path_wo = joinpath(HPM.BASE_DIR, "test/data/matpower/xfmr/two_bus_xfmr_YNyn0_hpf.m")
+        data_wo = PMs.parse_file(path_wo)
+        path_w  = joinpath(HPM.BASE_DIR, "test/data/matpower/xfmr/two_bus_xfmr_YNyn0_w_P0_hpf.m")
+        data_w  = PMs.parse_file(path_w)
+
+        # define the set of considered harmonics
+        H = [1]
+
+        # build the harmonic data
+        hdata_wo = HPM.build_hdata_from_matpower_file(data_wo, H=H)
+        hdata_w  = HPM.build_hdata_from_matpower_file(data_w, H=H)
+
+        # power flow
+        results_hpf_wo = HPM.solve_hpf(hdata_wo, HPM.HarmonicPowerModel, solver_nlp)
+        results_hpf_w  = HPM.solve_hpf(hdata_w, HPM.HarmonicPowerModel, solver_nlp)
+
+        # feasibility tests 
+        @test results_hpf_wo["termination_status"] == LOCALLY_SOLVED
+        @test results_hpf_w["termination_status"]  == LOCALLY_SOLVED
+
+        # equality tests
+        sol_hpf_wo = results_hpf_wo["solution"]["nw"]["1"]
+        sol_hpf_w  = results_hpf_w["solution"]["nw"]["1"]
+
+        pg_wo   =   sol_hpf_wo["bus"]["1"]["vbr"] * sol_hpf_wo["gen"]["1"]["cgr"] 
+                  + sol_hpf_wo["bus"]["1"]["vbi"] * sol_hpf_wo["gen"]["1"]["cgi"]
+        qg_wo   =   sol_hpf_wo["bus"]["1"]["vbi"] * sol_hpf_wo["gen"]["1"]["cgr"] 
+                  - sol_hpf_wo["bus"]["1"]["vbr"] * sol_hpf_wo["gen"]["1"]["cgi"]
+
+        pg_w    =   sol_hpf_w["bus"]["1"]["vbr"] * sol_hpf_w["gen"]["1"]["cgr"] 
+                  + sol_hpf_w["bus"]["1"]["vbi"] * sol_hpf_w["gen"]["1"]["cgi"]
+        qg_w    =   sol_hpf_w["bus"]["1"]["vbi"] * sol_hpf_w["gen"]["1"]["cgr"] 
+                  - sol_hpf_w["bus"]["1"]["vbr"] * sol_hpf_w["gen"]["1"]["cgi"]
+
+        pl_w    = data_w["load"]["1"]["pd"]
+        p0_w    = (sol_hpf_w["xfmr"]["1"]["exr"]^2 + sol_hpf_w["xfmr"]["1"]["exi"]^2) * data_w["xfmr"]["1"]["gsh"]
+        p1_w    = (sol_hpf_w["xfmr"]["1"]["cxr_1"]^2 + sol_hpf_w["xfmr"]["1"]["cxi_1"]^2) * data_w["xfmr"]["1"]["r1"]
+        p2_w    = (sol_hpf_w["xfmr"]["1"]["cxr_2"]^2 + sol_hpf_w["xfmr"]["1"]["cxi_2"]^2) * data_w["xfmr"]["1"]["r2"]
+
+        pl_wo   = data_wo["load"]["1"]["pd"]
+        p1_wo   = (sol_hpf_wo["xfmr"]["1"]["cxr_1"]^2 + sol_hpf_wo["xfmr"]["1"]["cxi_1"]^2) * data_wo["xfmr"]["1"]["r1"]
+        p2_wo   = (sol_hpf_wo["xfmr"]["1"]["cxr_2"]^2 + sol_hpf_wo["xfmr"]["1"]["cxi_2"]^2) * data_wo["xfmr"]["1"]["r2"]
+
+        @test pg_wo < pg_w
+        @test qg_wo ≈ qg_w
+
+        @test pg_w  ≈ pl_w + p0_w + p1_w + p2_w
+        @test pg_wo ≈ pl_wo + p1_wo + p2_wo
+    end
+
+    @testset "Two bus system, single xfmr, YNyn0 vs YNyn3" begin
+        # Example comparing a two bus system with a single xfmr with vector 
+        # group YNyn0 and YNyn3, active and reactive power consumption should 
+        # remain unchanged.
+        path_0 = joinpath(HPM.BASE_DIR, "test/data/matpower/xfmr/two_bus_xfmr_YNyn0_hpf.m")
+        data_0 = PMs.parse_file(path_0)
+        path_3 = joinpath(HPM.BASE_DIR, "test/data/matpower/xfmr/two_bus_xfmr_YNyn3_hpf.m")
+        data_3 = PMs.parse_file(path_3)
+
+        # define the set of considered harmonics
+        H = [1,2,3]
+
+        # build the harmonic data
+        hdata_0 = HPM.build_hdata_from_matpower_file(data_0, H=H)
+        hdata_3 = HPM.build_hdata_from_matpower_file(data_3, H=H)
+
+        # power flow
+        results_hpf_0 = HPM.solve_hpf(hdata_0, HPM.HarmonicPowerModel, solver_nlp)
+        results_hpf_3 = HPM.solve_hpf(hdata_3, HPM.HarmonicPowerModel, solver_nlp)
+
+        # feasibility tests 
+        @test results_hpf_0["termination_status"] == LOCALLY_SOLVED
+        @test results_hpf_3["termination_status"]  == LOCALLY_SOLVED
+
+        # equality tests
+        sol_hpf_0 = results_hpf_0["solution"]["nw"]["1"]
+        sol_hpf_3 = results_hpf_3["solution"]["nw"]["1"]
+
+        pg_0    =   sol_hpf_0["bus"]["1"]["vbr"] * sol_hpf_0["gen"]["1"]["cgr"] 
+                  + sol_hpf_0["bus"]["1"]["vbi"] * sol_hpf_0["gen"]["1"]["cgi"]
+        qg_0    =   sol_hpf_0["bus"]["1"]["vbi"] * sol_hpf_0["gen"]["1"]["cgr"] 
+                  - sol_hpf_0["bus"]["1"]["vbr"] * sol_hpf_0["gen"]["1"]["cgi"]
+
+        pg_3    =   sol_hpf_3["bus"]["1"]["vbr"] * sol_hpf_3["gen"]["1"]["cgr"] 
+                  + sol_hpf_3["bus"]["1"]["vbi"] * sol_hpf_3["gen"]["1"]["cgi"]
+        qg_3    =   sol_hpf_3["bus"]["1"]["vbi"] * sol_hpf_3["gen"]["1"]["cgr"] 
+                  - sol_hpf_3["bus"]["1"]["vbr"] * sol_hpf_3["gen"]["1"]["cgi"]
+
+        vba_0   = atand(sol_hpf_0["bus"]["2"]["vbi"],sol_hpf_0["bus"]["2"]["vbr"])
+        vba_3   = atand(sol_hpf_3["bus"]["2"]["vbi"],sol_hpf_3["bus"]["2"]["vbr"])
+
+        @test pg_0 ≈ pg_3
+        @test qg_0 ≈ qg_3
+
+        # The secondary winding is vg * 30° lagging with reference to the 
+        # primary, positive sense of rotation is counter-clockwise, i.e., 
+        # 1 o'clock is one hour behind 12 o'clock.
+        @test vba_0 ≈ vba_3 + 3*30
     end
 
 end
